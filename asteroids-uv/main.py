be9c3bae8226f290fed7e86d1669104b9f9f2e2c
Woody370 <pwoodstrup@gmail.com>
2025-10-23 11:46:33 -0500

    E: Added asteroid object and asteroid field. Changed color of ship and asteroids

--- asteroids-uv/main.py
+++ asteroids-uv/main.py
@@ -1,25 +1,49 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import *
 
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
     pygame.init()
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     dt = 0
+    
+    updatable_group = pygame.sprite.Group()
+    drawable_group = pygame.sprite.Group()
+    Player.containers = (updatable_group, drawable_group)
+
+    asteroids_group = pygame.sprite.Group()
+    Asteroid.containers = (updatable_group, drawable_group, asteroids_group)
+
+    AsteroidField.containers = (updatable_group)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    asteroid_field = AsteroidField()
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         dt = clock.tick(60) / 1000
-        player.update(dt)
+       
+        updatable_group.update(dt)
+        
         screen.fill("black")
-        player.draw(screen)
+       
+        for sprite in drawable_group:
+            sprite.draw(screen)
+        
         pygame.display.flip()
     
 
